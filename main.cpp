752f487162aac44d670d241115f038fa67e273a0
Daniel Antonio Quihuis <daniel.quihuish@gmail.com>
2022-07-14 01:40:47 -0700

    Archivos
    ...

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+  Nombre: main.cpp
+  Autor: Daniel Antonio Quihuis Hernandez
+  Fecha: julio del 2022
+  Descripcion: Este archivo contiene una funcion que se encarga de ejecutar todo el sitema
+*/
+
+
+/** \fn int *crear_arreglo(int L)
+ * \brief Crea un arreglo de tamaño L
+ * \param L Tamaño del arreglo.
+ * \return Devuelve el puntero al arreglo.  */
+ int main(/* no arguments */) {
+
+run();
+
+   cout << "Presione una tecla para continuar...\n" << endl;
+   cin.get();
+   return 0;
+ }
---
+++ utileria.h
@@ -0,0 +1,22 @@
+
+/*
+  Nombre: main.cpp
+  Autor: Daniel Antonio Quihuis Hernandez
+  Fecha: julio del 2022
+  Descripcion: Este archivo contiene una funcion que se encarga de ejecutar todo el sitema
+*/
+
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+int run(){
+
+cout << endl;
+cout <<
+
+  return 0;
+}
